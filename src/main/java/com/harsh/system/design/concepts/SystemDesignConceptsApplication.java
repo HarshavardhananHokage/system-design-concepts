d9706f4fd9ccc9196746cf5decb552ae0061cde9
Harshavardhanan <harshavardhan2990@gmail.com>
2025-04-22 13:24:14 +0530

    first commit

---
+++ src/main/java/com/harsh/system/design/concepts/SystemDesignConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.harsh.system.design.concepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemDesignConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemDesignConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harsh/system/design/concepts/SystemDesignConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.system.design.concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemDesignConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
